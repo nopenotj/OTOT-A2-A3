115afbca434e0dfe403ddc7ecbb6887fd386472f
Jay <Nopenotj@gmail.com>
2022-09-18 17:45:58 +0800

    A3.2

--- k8s/a3_setup.sh
+++ k8s/a3_setup.sh
@@ -7,5 +7,13 @@
 # kubectl -nkube-system edit deploy/metrics-server
 # kubectl -nkube-system rollout restart deploy/metrics-server
 
+# A3.1
 kubectl apply -f ./k8s/manifests/backend-metrics-service.yaml
 kubectl apply -f ./k8s/manifests/backend-hpa.yaml
+
+# A3.2
+kubectl apply -f ./k8s/manifests/ backend-deployment-zone-aware.yaml
+
+# Verify
+kubectl get nodes -L topology.kubernetes.io/zone
+kubectl get po -lapp=backend-zone-aware -owide --sort-by='.spec.nodeName'
